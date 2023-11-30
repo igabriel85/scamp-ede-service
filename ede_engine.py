438551a058337cd2445d5719088491f032de2fea
Silviu Panica <silviu@innoqube.ro>
2023-11-30 21:02:15 +0200

    fixed the timestamp precision

--- ede_engine.py
+++ ede_engine.py
@@ -188,8 +188,8 @@ class EDEScampEngine():
             for cycle in body['cycles']:
                 # cycle['device_id'] = device_id
                 cycle['node'] = device_id
-                cycle['cycle_start'] = cycle['start'].timestamp()
-                cycle['cycle_end'] = cycle['end'].timestamp()
+                cycle['cycle_start'] = cycle['start'].timestamp()*1000
+                cycle['cycle_end'] = cycle['end'].timestamp()*1000
                 cycle['cycle_type'] = int(cycle['cycle_type'])
                 if cycle['anomaly'] is None:
                     cycle['anomaly'] = ''
